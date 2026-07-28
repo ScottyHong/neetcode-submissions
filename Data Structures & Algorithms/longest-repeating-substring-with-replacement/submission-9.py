a67e770aac54007cde8f5a23dccbc6d26cd29117
Scotty Hong <136658939+ScottyHong@users.noreply.github.com>
2026-07-28 00:00:33 -0400

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.py
@@ -0,0 +1,29 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        L,R = 0,0
+        maxFreq = 0
+        result = 0
+        count = {}
+
+        while R < len(s):
+            window_length = R - L + 1
+            char = s[R]
+            
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+
+            maxFreq = max(maxFreq, count[char])
+            #We need to check if the condition is true
+            print(window_length)
+            print(maxFreq)
+            if window_length - maxFreq <= k:
+                result = max(window_length, result)
+                R += 1
+                print(f'condition holds: {result}')
+            else:
+                count[s[R]] -= 1
+                count[s[L]] -= 1
+                L += 1
+        return result
